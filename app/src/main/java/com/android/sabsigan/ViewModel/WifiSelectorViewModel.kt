e0a9110ca53a5a42e6189aa7bf22b50f91e622d5
marso34 <mars102aq@gmail.com>
2023-11-10 15:21:06 +0900

    파이어베이스 세팅

--- app/src/main/java/com/android/sabsigan/ViewModel/WifiSelectorViewModel.kt
+++ app/src/main/java/com/android/sabsigan/ViewModel/WifiSelectorViewModel.kt
@@ -1,4 +1,8 @@
 package com.android.sabsigan.ViewModel
 
+import androidx.databinding.ObservableField
+
 class WifiSelectorViewModel: WiFiViewModel() {
+    var iconColor = ObservableField<String>()
+    var shadowColor = ObservableField<String>()
 }
